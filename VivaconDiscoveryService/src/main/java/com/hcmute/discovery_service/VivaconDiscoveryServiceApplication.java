a35ec247ef103c5f6410537f8b80565828ac8d26
hungdoan-tech <hungdoan426@gmail.com>
2021-09-21 11:01:13 +0700

    Implement a simple Discovery Service

---
+++ VivaconDiscoveryService/src/main/java/com/hcmute/discovery_service/VivaconDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.hcmute.discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class VivaconDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivaconDiscoveryServiceApplication.class, args);
+	}
+
+}
